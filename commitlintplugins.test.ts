9e930674897ee6d570ecd09fde6da0592a8510cb
Andres G. Aragoneses <knocte@gmail.com>
2022-10-02 17:03:53 +0800

    plugins.test: refactor for DRY

--- commitlintplugins.test.ts
+++ commitlintplugins.test.ts
@@ -1,71 +1,75 @@
 const { spawnSync } = require('child_process');
 
+function runCommitLintOnMsg(inputMsg: string) {
+    return spawnSync('npx', ['commitlint', '--verbose'], { input: inputMsg });
+}
+
 test('type-space-after-colon1', () => {
     let commitMsgWithNoSpace = 'foo:bar';
-    let typeSpaceAfterColon1 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithNoSpace });
+    let typeSpaceAfterColon1 = runCommitLintOnMsg(commitMsgWithNoSpace);
     expect(typeSpaceAfterColon1.status).not.toBe(0);
 });
 
 
 test('type-space-after-colon2', () => {
     let commitMsgWithSpace = 'foo: bar';
-    let typeSpaceAfterColon2 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithSpace });
+    let typeSpaceAfterColon2 = runCommitLintOnMsg(commitMsgWithSpace);
     expect(typeSpaceAfterColon2.status).toBe(0);
 });
 
 
 test('type-space-after-colon3', () => {
     let commitMsgWithNoSpaceBeforeColonButAtTheEnd = 'foo: a tale of bar:baz';
-    let typeSpaceAfterColon3 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithNoSpaceBeforeColonButAtTheEnd });
+    let typeSpaceAfterColon3 = runCommitLintOnMsg(commitMsgWithNoSpaceBeforeColonButAtTheEnd);
     expect(typeSpaceAfterColon3.status).toBe(0);
 });
 
 
 test('subject-lowercase1', () => {
     let commitMsgWithUppercaseAfterColon = "foo: Bar baz";
-    let subjectLowerCase1 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithUppercaseAfterColon });
+    let subjectLowerCase1 = runCommitLintOnMsg(commitMsgWithUppercaseAfterColon);
     expect(subjectLowerCase1.status).not.toBe(0);
 });
 
 
 test('subject-lowercase2', () => {
     let commitMsgWithLowercaseAfterColon = "foo: bar baz";
-    let subjectLowerCase2 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithLowercaseAfterColon });
+    let subjectLowerCase2 = runCommitLintOnMsg(commitMsgWithLowercaseAfterColon);
     expect(subjectLowerCase2.status).toBe(0);
 });
 
 
 test('subject-lowercase3', () => {
     let commitMsgWithAcronymAfterColon = "foo: BAR baz";
-    let subjectLowerCase3 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithAcronymAfterColon });
+    let subjectLowerCase3 = runCommitLintOnMsg(commitMsgWithAcronymAfterColon);
     expect(subjectLowerCase3.status).toBe(0);
 });
 
 
 test('subject-lowercase4', () => {
     let commitMsgWithNonAlphanumericAfterColon = "foo: 3 tests added";
-    let subjectLowerCase4 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithNonAlphanumericAfterColon });
+    let subjectLowerCase4 = runCommitLintOnMsg(commitMsgWithNonAlphanumericAfterColon);
     expect(subjectLowerCase4.status).toBe(0);
 });
 
 
 test('subject-lowercase5', () => {
     let commitMsgWithRareCharInArea1 = "foo.bar: Baz";
-    let subjectLowerCase5 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithRareCharInArea1 });
+    let subjectLowerCase5 = runCommitLintOnMsg(commitMsgWithRareCharInArea1);
     expect(subjectLowerCase5.status).not.toBe(0);
 });
 
 
 test('subject-lowercase6', () => {
     let commitMsgWithRareCharInArea2 = "foo-bar: Baz";
-    let subjectLowerCase6 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithRareCharInArea2 });
+    let subjectLowerCase6 = runCommitLintOnMsg(commitMsgWithRareCharInArea2);
     expect(subjectLowerCase6.status).not.toBe(0);
 });
 
 
 test('subject-lowercase7', () => {
     let commitMsgWithRareCharInArea3 = "foo,bar: Baz";
-    let subjectLowerCase7 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithRareCharInArea3 });
+    let subjectLowerCase7 = runCommitLintOnMsg(commitMsgWithRareCharInArea3);
     //console.log("=============>" + subjectLowerCase7.stdout);
     expect(subjectLowerCase7.status).not.toBe(0);
 });
