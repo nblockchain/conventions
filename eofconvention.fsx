e960934e680f09a6a758a473b2be734bc19e9fdf
realmarv <zahratehraninasab@gmail.com>
2022-10-31 16:39:38 +0330

    ci: run eofconvention in ci

--- eofconvention.fsx
+++ eofconvention.fsx
@@ -1,29 +1,26 @@
 open System.IO
 
-let EOFIsEOL(path: FileInfo) = 
-    try
-        use sr = new StreamReader (path.FullName)
-        let filetext = sr.ReadToEnd()
+let EolAtEof(path: FileInfo) = 
+
+    use streamReader = new StreamReader (path.FullName)
+    let filetext = streamReader.ReadToEnd()
+    
+    if filetext <> "" then
         (filetext |> Seq.last = '\n')
-        
-    with
-        | :? System.ArgumentException -> printfn "%A" path; true
-        
+    else
+        true
 
-let EOFIsNotEOL(path: FileInfo) = not (EOFIsEOL path)
-
-let NotPrefix (value: string) (str: string) =
-    not (str.StartsWith(value))
-
+let NotPrefix (prefix: string) (mainStr: string) =
+    not (mainStr.StartsWith(prefix))
 
 let invalidFiles = 
     Directory.GetFiles(".", "*.*", SearchOption.AllDirectories) 
-    |> Array.filter (NotPrefix "./node_modules")
-    |> Array.filter (NotPrefix "./.git")
-    |> Array.map (fun x -> FileInfo(x))
-    |> Array.filter EOFIsNotEOL
+    |> Seq.filter (NotPrefix "./node_modules")
+    |> Seq.filter (NotPrefix "./.git")
+    |> Seq.map (fun pathStr -> FileInfo pathStr)
+    |> Seq.filter (fun fileInfo -> not (EolAtEof fileInfo))
 
-if invalidFiles.Length > 0 then
+if invalidFiles |> Seq.length > 0 then
     let mutable message = "The following files doesn't end with EOL:\n" 
-    invalidFiles |> Array.iter (fun x -> message <- message + x.FullName + "\n")
-    raise (System.Exception(message))
+    invalidFiles |> Seq.iter (fun x -> message <- message + x.FullName + "\n")
+    failwith message
