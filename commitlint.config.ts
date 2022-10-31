53bec64d4667b75b86a10953f015c14229572a5f
realmarv <zahratehraninasab@gmail.com>
2022-10-31 15:35:09 +0330

    commitlint.config: simplify

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -574,18 +574,14 @@ module.exports = {
                     }
 
                     // taken from https://stackoverflow.com/a/66433444/544947 and https://unix.stackexchange.com/a/25208/56844
-                    function getUnixCommand(fmt_option: string){
-                        fmt_option = fmt_option.trim();
-                        if (fmt_option !== '') { 
-                            fmt_option = ' ' + fmt_option;
-                        }
-                        return `git log --format=%B -n 1 $(git log -1 --pretty=format:"%h") | cat - > log.txt ; fmt -w 1111 -s log.txt > ulog.txt && fmt -w 64 -s${fmt_option} ulog.txt > wlog.txt && git commit --amend -F wlog.txt`;
+                    function getUnixCommand(fmtOption: string){
+                        return `git log --format=%B -n 1 $(git log -1 --pretty=format:"%h") | cat - > log.txt ; fmt -w 1111 -s log.txt > ulog.txt && fmt -w 64 -s${fmtOption} ulog.txt > wlog.txt && git commit --amend -F wlog.txt`;
                     }
 
                     return [
                         !offence,
                         `Please do not exceed ${bodyMaxLineLength} characters in the lines of the commit message's body; we recommend this unix command (for editing the last commit message): \n` +
-                        `For Linux users: ${getUnixCommand('-u')}\n` +
+                        `For Linux users: ${getUnixCommand(' -u')}\n` +
                         `For macOS users: ${getUnixCommand('')}`
                     ];
                 },
