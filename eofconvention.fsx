0a07209b347ef36beace2d3e1ac4232a56f42636
realmarv <zahratehraninasab@gmail.com>
2022-10-31 14:04:18 +0330

    eofconvention: filter autogenerated files

--- eofconvention.fsx
+++ eofconvention.fsx
@@ -1,18 +1,29 @@
 open System.IO
 
-let EOFIsEOL(path: string) = 
+let EOFIsEOL(path: FileInfo) = 
     try
-        use sr = new StreamReader (path)
+        use sr = new StreamReader (path.FullName)
         let filetext = sr.ReadToEnd()
         (filetext |> Seq.last = '\n')
         
-    with ex ->
-        // printfn "%A" ex
-        true
-    
-let EOFIsNotEOL(path: string) = not (EOFIsEOL path)
+    with
+        | :? System.ArgumentException -> printfn "%A" path; true
+        
 
-printfn "%A" (
-    Directory.GetFiles("./", "*.*", SearchOption.AllDirectories) 
+let EOFIsNotEOL(path: FileInfo) = not (EOFIsEOL path)
+
+let NotPrefix (value: string) (str: string) =
+    not (str.StartsWith(value))
+
+
+let invalidFiles = 
+    Directory.GetFiles(".", "*.*", SearchOption.AllDirectories) 
+    |> Array.filter (NotPrefix "./node_modules")
+    |> Array.filter (NotPrefix "./.git")
+    |> Array.map (fun x -> FileInfo(x))
     |> Array.filter EOFIsNotEOL
-    )
+
+if invalidFiles.Length > 0 then
+    let mutable message = "The following files doesn't end with EOL:\n" 
+    invalidFiles |> Array.iter (fun x -> message <- message + x.FullName + "\n")
+    raise (System.Exception(message))
