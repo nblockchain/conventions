4cc9be007dcc52dbdae8d21dec3858ee5611bfe0
realmarv <zahratehraninasab@gmail.com>
2022-11-10 15:20:10 +0330

    shebangConvention: find fsx files without shebang

---
+++ shebangConvention.fsx
@@ -0,0 +1,26 @@
+#!/usr/bin/env fsx
+
+open System
+open System.IO
+#load "fileConventions/Library.fs"
+
+let NotInDir (dirName: string) (fileInfo: FileInfo) =
+    not (fileInfo.FullName.Contains $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}")
+
+let invalidFiles = 
+    Directory.GetFiles(".", "*.fsx", SearchOption.AllDirectories) 
+    |> Seq.map (fun pathStr -> FileInfo pathStr)
+    |> Seq.filter (NotInDir "node_modules")
+    |> Seq.filter (NotInDir ".git")
+    |> Seq.filter (fun fileInfo -> not (fileInfo.Name = "DummyWithoutShebang.fsx"))
+    |> Seq.filter (fun fileInfo -> not (fileConventions.HasShebang fileInfo))
+
+if Seq.length invalidFiles > 0 then
+    let message = 
+        "The following files don't have shebang:" + 
+        Environment.NewLine + 
+        (invalidFiles 
+        |> Seq.map (fun fileInfo -> fileInfo.FullName)
+        |> String.concat Environment.NewLine)
+        
+    failwith message
