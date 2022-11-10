c2be469137ba2f66569f2de941010e706b49a0c4
realmarv <zahratehraninasab@gmail.com>
2022-11-10 12:19:39 +0330

    fileConventions: implement HasShebang

--- fileConventions/Library.fs
+++ fileConventions/Library.fs
@@ -3,7 +3,7 @@ open System.IO
 open System
 
 let HasShebang (fileInfo: FileInfo) =
-    true
-    // use streamReader = new StreamReader (fileInfo.FullName)
-    // let fileText = streamReader.ReadToEnd()
-    // fileText.[..2] = "#!"
+    use streamReader = new StreamReader (fileInfo.FullName)
+    let fileText = streamReader.ReadToEnd()
+    fileText.[..2] = "#!"
+
