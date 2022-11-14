a0d39747bb56078ca3fd568a583f4fc081a46077
realmarv <zahratehraninasab@gmail.com>
2022-11-14 12:40:16 +0330

    FileConventions.Test: move dummy files to a folder

--- src/FileConventions.Test/FileConventions.Test.fs
+++ src/FileConventions.Test/FileConventions.Test.fs
@@ -12,29 +12,29 @@ let Setup () =
 
 [<Test>]
 let HasCorrectShebangTest1 () =
-    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyWithoutShebang.fsx")
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyFiles{Path.DirectorySeparatorChar}DummyWithoutShebang.fsx")
     Assert.IsFalse(HasCorrectShebang(fileInfo))
 
 
 [<Test>]
 let HasCorrectShebangTest2 () =
-    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyWithShebang.fsx")
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyFiles{Path.DirectorySeparatorChar}DummyWithShebang.fsx")
     Assert.IsTrue(HasCorrectShebang(fileInfo))
 
 
 [<Test>]
 let HasCorrectShebangTest3 () =
-    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyWithWrongShebang.fsx")
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyFiles{Path.DirectorySeparatorChar}DummyWithWrongShebang.fsx")
     Assert.IsFalse(HasCorrectShebang(fileInfo))
 
 
 [<Test>]
 let HasCorrectShebangTest4() =
-    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyEmpty.fsx")
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyFiles{Path.DirectorySeparatorChar}DummyEmpty.fsx")
     Assert.IsFalse(HasCorrectShebang(fileInfo))
 
 
 [<Test>]
 let IsExecutable () =
-    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyExecutable.fsx")
+    let fileInfo = (FileInfo $"{__SOURCE_DIRECTORY__}{Path.DirectorySeparatorChar}DummyFiles{Path.DirectorySeparatorChar}DummyExecutable.fsx")
     Assert.Fail()
