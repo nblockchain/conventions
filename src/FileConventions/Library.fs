3918e0219e52e14370219c215b2ec407d99bd2c6
realmarv <zahratehraninasab@gmail.com>
2022-11-15 12:03:40 +0330

    FileConventions: remove extra parens

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -11,10 +11,10 @@ let HasCorrectShebang (fileInfo: FileInfo) =
     let fileText = File.ReadLines(fileInfo.FullName)
     if fileText.Any() then
         let firstLine = fileText.First()
-        (
-            firstLine.StartsWith("#!/usr/bin/env fsx") || 
-            firstLine.StartsWith("#!/usr/bin/env -S dotnet fsi")
-        )
+        
+        firstLine.StartsWith "#!/usr/bin/env fsx" || 
+        firstLine.StartsWith "#!/usr/bin/env -S dotnet fsi"
+        
     else
         false
 
