eeeca87b3785a5a37b75d92f5827f298207381dd
realmarv <zahratehraninasab@gmail.com>
2022-11-17 16:08:34 +0330

    eofConvention: add a whitelist

--- eofConvention.fsx
+++ eofConvention.fsx
@@ -13,12 +13,15 @@ let EolAtEof(fileInfo: FileInfo) =
 let NotInDir (dirName: string) (fileInfo: FileInfo) =
     not (fileInfo.FullName.Contains $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}")
 
+let whitelist = [".svg"; ".png"; ".slnf"]
+
 let invalidFiles = 
     Directory.GetFiles(".", "*.*", SearchOption.AllDirectories) 
     |> Seq.map (fun pathStr -> FileInfo pathStr)
     |> Seq.filter (NotInDir "node_modules")
     |> Seq.filter (NotInDir ".git")
     |> Seq.filter (fun fileInfo -> not (EolAtEof fileInfo))
+    |> Seq.filter (fun fileInfo -> not (List.contains fileInfo.Extension whitelist))
 
 if Seq.length invalidFiles > 0 then
     let message = 
