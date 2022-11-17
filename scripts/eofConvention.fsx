7b4a78438af93c9bf1ef2c23133804c325cb776c
realmarv <zahratehraninasab@gmail.com>
2022-11-17 16:08:34 +0330

    scripts/eofConvention.fsx: ignore binary files

--- scripts/eofConvention.fsx
+++ scripts/eofConvention.fsx
@@ -1,6 +1,8 @@
 open System.IO
 open System
 
+#load "../src/FileConventions/Library.fs"
+
 let EolAtEof(fileInfo: FileInfo) = 
     use streamReader = new StreamReader (fileInfo.FullName)
     let filetext = streamReader.ReadToEnd()
@@ -10,15 +12,21 @@ let EolAtEof(fileInfo: FileInfo) =
     else
         true
 
-let NotInDir (dirName: string) (fileInfo: FileInfo) =
-    not (fileInfo.FullName.Contains $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}")
+let InDir (dirName: string) (fileInfo: FileInfo) = 
+    fileInfo.FullName.Contains $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}"
+
+let NotInDirs (dirNames: List<string>) (fileInfo: FileInfo) =
+    not (dirNames 
+        |> Seq.map (fun dirName -> InDir dirName fileInfo)
+        |> Seq.contains true)
+
+let whitelistFolders = ["node_modules"; ".git"; "Debug"; "obj"; "bin"; "DummyFiles"]
 
 let invalidFiles = 
     Directory.GetFiles(".", "*.*", SearchOption.AllDirectories) 
     |> Seq.map (fun pathStr -> FileInfo pathStr)
-    |> Seq.filter (NotInDir "node_modules")
-    |> Seq.filter (NotInDir ".git")
-    |> Seq.filter (NotInDir "DummyFiles")
+    |> Seq.filter (NotInDirs whitelistFolders)
+    |> Seq.filter (fun fileInfo -> not (FileConventions.HasBinaryContent fileInfo))
     |> Seq.filter (fun fileInfo -> not (EolAtEof fileInfo))
 
 if Seq.length invalidFiles > 0 then
