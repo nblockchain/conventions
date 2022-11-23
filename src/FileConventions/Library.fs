2875d0351b8f18d986dbd69056f62dfabf0ad014
realmarv <zahratehraninasab@gmail.com>
2022-11-23 16:44:22 +0330

    FileConventions: use File.ReadLines

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -5,6 +5,7 @@ open System.IO
 open System.Linq
 
 let HasBinaryContent (fileInfo: FileInfo) =
-    use streamReader = new StreamReader (fileInfo.FullName)
-    let content = streamReader.ReadToEnd()
-    content.Any(fun ch -> Char.IsControl(ch) && ch <> '\r' && ch <> '\n')
+    let lines = File.ReadLines fileInfo.FullName 
+    lines 
+        |> Seq.map (fun line -> line.Any(fun character -> Char.IsControl(character) && character <> '\r' && character <> '\n'))
+        |> Seq.contains true 
