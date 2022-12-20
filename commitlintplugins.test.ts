b579ca94a0e2d67291cebc97ac38e1f7cec78468
realmarv <zahratehraninasab@gmail.com>
2022-12-20 18:41:21 +0330

    commitlintplugins.test: remove line

--- commitlintplugins.test.ts
+++ commitlintplugins.test.ts
@@ -190,7 +190,6 @@ replacing generic try-with which lead to a red CI in the
 PR, we had to define its special excpetion and catch that:
 \`\`\`
    The active test run was aborted. Reason: Test host process crashed : Unhandled exception. System.Exception: Key handshake failed!
-   at <StartupCode$FSharp-Core>.$Mailbox.processFirstArrival@303-8.Invoke(AsyncActivation\`1 ctxt) in F:\\workspace\\_work\\1\\s\\src\\fsharp\\FSharp.Core\\mailbox.fs:line 303
    at Microsoft.FSharp.Control.Trampoline.Execute(FSharpFunc\`2 firstAction) in F:\\workspace\\_work\\1\\s\\src\\fsharp\\FSharp.Core\\async.fs:line 109
 --- End of stack trace from previous location where exception was thrown ---
    at Microsoft.FSharp.Control.AsyncPrimitives.Start@907-1.Invoke(ExceptionDispatchInfo edi) in F:\\workspace\\_work\\1\\s\\src\\fsharp\\FSharp.Core\\async.fs:line 907
