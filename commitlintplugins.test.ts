b16a37ecdddb0caa74f5f7ea6f9da6d3e235b730
realmarv <zahratehraninasab@gmail.com>
2022-12-21 17:09:55 +0330

    commitlintplugins.test: remove block signs

--- commitlintplugins.test.ts
+++ commitlintplugins.test.ts
@@ -188,9 +188,7 @@ Previously, TorHandshakes used failwith to raise handshake
 failed error. Since we need to catch it, as a result of
 replacing generic try-with which lead to a red CI in the
 PR, we had to define its special excpetion and catch that:
-\`\`\`
 --- End of stack trace from previous location where exception was thrown ---
-\`\`\`
 A very long line. A very long line. A very long line. A very long line. A very long line. A very long line.
 `
     let bodyMaxLineLength8 = runCommitLintOnMsg(commitMsgWithLargeBody);
