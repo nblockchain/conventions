bb417c41d6c92be7473023ecd1e71cd4554deeb0
Andres G. Aragoneses <knocte@gmail.com>
2023-02-12 09:55:28 +0100

    baz.baz: update

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -9,7 +9,7 @@ let footerMaxLineLength = 150;
 module.exports = {
     parserPreset: "conventional-changelog-conventionalcommits",
     rules: {
-        "type-enum": [RuleConfigSeverity.Error, "always", ["foo", "bar"]],
+        "type-enum": [RuleConfigSeverity.Error, "always", ["foo", "bar", "baz.baz"]],
         "body-leading-blank": [RuleConfigSeverity.Error, "always"],
         "body-soft-max-line-length": [
             RuleConfigSeverity.Error,
