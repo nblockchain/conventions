b6e8047ffd05847f0c009e39170af33e8d2cedcf
Andres G. Aragoneses <knocte@gmail.com>
2023-02-15 13:16:16 +0100

    GitHubCI: call also the mixedLineEndings F# script

--- scripts/mixedLineEndings.fsx
+++ scripts/mixedLineEndings.fsx
@@ -1,11 +1,11 @@
 open System.IO
 open System
 
-let MixedLineEndings(fileInfo: FileInfo) = 
+let MixedLineEndings(fileInfo: FileInfo) =
     use streamReader = new StreamReader (fileInfo.FullName)
     let fileText = streamReader.ReadToEnd()
 
-    let numberOfLineEndings = ([| "\n"; "\r\n"; "\r" |] 
+    let numberOfLineEndings = ([| "\n"; "\r\n"; "\r" |]
         |> Seq.filter(function ending -> fileText.IndexOf(ending) > 0)
         |> Seq.length)
 
@@ -15,18 +15,18 @@ let NotInDir (dirName: string) (fileInfo: FileInfo) =
     not (fileInfo.FullName.Contains $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}")
 
 let invalidFiles = 
-    Directory.GetFiles(".", "*.*", SearchOption.AllDirectories) 
+    Directory.GetFiles(".", "*.*", SearchOption.AllDirectories)
     |> Seq.map (fun pathStr -> FileInfo pathStr)
     |> Seq.filter (NotInDir "node_modules")
     |> Seq.filter (NotInDir ".git")
     |> Seq.filter MixedLineEndings
 
 if Seq.length invalidFiles > 0 then
-    let message = 
-        "The following files shouldn't use mixed line endings:" + 
-        Environment.NewLine + 
-        (invalidFiles 
+    let message =
+        "The following files shouldn't use mixed line endings:" +
+        Environment.NewLine +
+        (invalidFiles
         |> Seq.map (fun fileInfo -> fileInfo.FullName)
         |> String.concat Environment.NewLine)
-        
+
     failwith message
