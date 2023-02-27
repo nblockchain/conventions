146fc5804544b850af138793c901ac185a299957
realmarv <zahratehraninasab@gmail.com>
2023-02-27 10:32:21 +0330

    package.json: add, to downgrade cosmiconfig
    Otherwise we get this error in the CI:
    ```
    + npx commitlint --version
    /__w/conventions/conventions/node_modules/@commitlint/load/node_modules/cosmiconfig/dist/index.js:63
      const config = metaConfig.config ?? {};
                                        ^
    
    SyntaxError: Unexpected token '?'
        at wrapSafe (internal/modules/cjs/loader.js:915:16)
        at Module._compile (internal/modules/cjs/loader.js:963:27)
        at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)
        at Module.load (internal/modules/cjs/loader.js:863:32)
        at Function.Module._load (internal/modules/cjs/loader.js:708:14)
        at Module.require (internal/modules/cjs/loader.js:887:19)
        at require (internal/modules/cjs/helpers.js:74:18)
        at Object.<anonymous> (/__w/conventions/conventions/node_modules/@commitlint/load/lib/utils/load-config.js:7:23)
        at Module._compile (internal/modules/cjs/loader.js:999:30)
        at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)
    ```

--- commitlint.sh
+++ commitlint.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 # cd to directory of this script
 cd "$(dirname "$0")"
+npm install
 npm install conventional-changelog-conventionalcommits
 npm install commitlint@latest
 npx commitlint --version
