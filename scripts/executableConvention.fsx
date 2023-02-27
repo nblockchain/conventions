851e604cfe847c9aea8e3163cabe9ce798cc57fc
realmarv <zahratehraninasab@gmail.com>
2023-02-27 11:26:43 +0330

    scripts/executableConvention: add

---
+++ scripts/executableConvention.fsx
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System
+open System.IO
+
+#r "nuget: Mono.Unix, Version=7.1.0-final.1.21458.1"
+#load "../src/FileConventions/Library.fs"
+#load "../src/FileConventions/Helpers.fs"
+
+let rootDir = Path.Combine(__SOURCE_DIRECTORY__, "..") |> DirectoryInfo
+
+let invalidFiles =
+    Helpers.GetInvalidFiles
+        rootDir
+        "*.fsx"
+        (fun fileInfo -> not(FileConventions.IsExecutable fileInfo))
+
+Helpers.AssertNoInvalidFiles
+    invalidFiles
+    "The following files don't have execute permission:"
