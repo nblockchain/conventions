3c90f5292c821d02c51ca2b69ea0d682f950ce40
realmarv <zahratehraninasab@gmail.com>
2023-02-27 13:56:55 +0330

    FileConventions/Helpers: add
    Transfer duplicate code to Helpers.

--- scripts/mixedLineEndings.fsx
+++ scripts/mixedLineEndings.fsx
@@ -1,27 +1,14 @@
 #!/usr/bin/env -S dotnet fsi
 
-open System.IO
 open System
+open System.IO
 
 #load "../src/FileConventions/Library.fs"
+#load "../src/FileConventions/Helpers.fs"
 
-let NotInDir (dirName: string) (fileInfo: FileInfo) =
-    not (fileInfo.FullName.Contains $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}")
+let invalidFiles =
+    Helpers.GetInvalidFiles "." "*.*" FileConventions.MixedLineEndings
 
-let invalidFiles = 
-    Directory.GetFiles(".", "*.*", SearchOption.AllDirectories)
-    |> Seq.map (fun pathStr -> FileInfo pathStr)
-    |> Seq.filter (NotInDir "node_modules")
-    |> Seq.filter (NotInDir ".git")
-    |> Seq.filter(NotInDir "DummyFiles")
-    |> Seq.filter FileConventions.MixedLineEndings
-
-if Seq.length invalidFiles > 0 then
-    let message =
-        "The following files shouldn't use mixed line endings:" +
-        Environment.NewLine +
-        (invalidFiles
-        |> Seq.map (fun fileInfo -> fileInfo.FullName)
-        |> String.concat Environment.NewLine)
-
-    failwith message
+Helpers.AssertNoInvalidFiles
+    invalidFiles
+    "The following files shouldn't use mixed line endings:"
--- scripts/shebangConvention.fsx
+++ scripts/shebangConvention.fsx
@@ -4,31 +4,14 @@ open System
 open System.IO
 
 #load "../src/FileConventions/Library.fs"
-
-let NotInDir (dirName: string) (fileInfo: FileInfo) =
-    not(
-        fileInfo.FullName.Contains
-            $"%c{Path.DirectorySeparatorChar}%s{dirName}%c{Path.DirectorySeparatorChar}"
-    )
+#load "../src/FileConventions/Helpers.fs"
 
 let invalidFiles =
-    Directory.GetFiles(".", "*.fsx", SearchOption.AllDirectories)
-    |> Seq.map(fun pathStr -> FileInfo pathStr)
-    |> Seq.filter(NotInDir "node_modules")
-    |> Seq.filter(NotInDir ".git")
-    |> Seq.filter(NotInDir "bin")
-    |> Seq.filter(NotInDir "obj")
-    |> Seq.filter(NotInDir "DummyFiles")
-    |> Seq.filter(fun fileInfo ->
-        not(FileConventions.HasCorrectShebang fileInfo)
-    )
+    Helpers.GetInvalidFiles
+        "."
+        "*.fsx"
+        (fun fileInfo -> not(FileConventions.HasCorrectShebang fileInfo))
 
-if Seq.length invalidFiles > 0 then
-    let message =
-        "The following files don't have the correct shebang:"
-        + Environment.NewLine
-        + (invalidFiles
-           |> Seq.map(fun fileInfo -> fileInfo.FullName)
-           |> String.concat Environment.NewLine)
-
-    failwith message
+Helpers.AssertNoInvalidFiles
+    invalidFiles
+    "The following files should have the correct shebang:"
