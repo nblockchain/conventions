3aa5780fb41d15b91b7f8705d1cd4e7d36b50528
realmarv <zahratehraninasab@gmail.com>
2023-03-11 14:17:18 +0330

    FileConventions: use seq<FileInfo> input type
    Use seq<FileInfo> input type for
    DetectInconsistentVersionsInGitHubCI
    function.

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -105,22 +105,35 @@ let EolAtEof(fileInfo: FileInfo) =
             True
 
 let DetectInconsistentVersionsInGitHubCI(fileInfos: seq<FileInfo>) =
-    let fileInfo = Seq.nth 0 fileInfos
-    assert (fileInfo.FullName.EndsWith ".yml")
-    let fileLines = File.ReadLines fileInfo.FullName
+    fileInfos
+    |> Seq.iter(fun fileInfo -> assert (fileInfo.FullName.EndsWith ".yml"))
 
-    let pulumiVersions =
-        fileLines
-        |> Seq.filter(fun line -> line.Contains "pulumi-version:")
-        |> Seq.map(fun line -> line.Substring(line.IndexOf(":") + 1))
-        |> Seq.map(fun line -> line.Trim())
-        |> Set.ofSeq
+    let inconsistentPulumiVersions =
+        fileInfos
+        |> Seq.map(fun fileInfo -> File.ReadLines fileInfo.FullName)
+        |> Seq.map(fun fileLines ->
+            fileLines
+            |> Seq.filter(fun line -> line.Contains "pulumi-version:")
+            |> Seq.map(fun line -> line.Substring(line.IndexOf(":") + 1))
+            |> Seq.map(fun line -> line.Trim())
+            |> Set.ofSeq
+            |> Seq.length
+            |> (fun length -> length > 1)
+        )
+        |> Seq.contains true
 
-    let setupPulumiVersions =
-        fileLines
-        |> Seq.filter(fun line -> line.Contains "setup-pulumi@")
-        |> Seq.map(fun line -> line.Substring(line.IndexOf("@") + 1))
-        |> Seq.map(fun line -> line.Trim())
-        |> Set.ofSeq
+    let inconsistentSetupPulumiVersions =
+        fileInfos
+        |> Seq.map(fun fileInfo -> File.ReadLines fileInfo.FullName)
+        |> Seq.map(fun fileLines ->
+            fileLines
+            |> Seq.filter(fun line -> line.Contains "setup-pulumi@")
+            |> Seq.map(fun line -> line.Substring(line.IndexOf(":") + 1))
+            |> Seq.map(fun line -> line.Trim())
+            |> Set.ofSeq
+            |> Seq.length
+            |> (fun length -> length > 1)
+        )
+        |> Seq.contains true
 
-    Seq.length pulumiVersions > 1 || Seq.length setupPulumiVersions > 1
+    inconsistentPulumiVersions || inconsistentSetupPulumiVersions
