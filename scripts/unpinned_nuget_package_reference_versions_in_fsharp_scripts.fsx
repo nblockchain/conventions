06b2c65c216d66899704ccec373579abda3352ee
realmarv <zahratehraninasab@gmail.com>
2023-03-29 15:49:56 +0330

    scripts: add new script
    Add not_respecting_some_naming_conventions.fsx script to
    detect if some naming conventions are respected.
    
    Fixes https://github.com/nblockchain/conventions/issues/93

---
+++ scripts/unpinned_nuget_package_reference_versions_in_fsharp_scripts.fsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System
+open System.IO
+
+#r "nuget: Mono.Unix, Version=7.1.0-final.1.21458.1"
+#r "nuget: YamlDotNet, Version=16.1.3"
+
+#load "../src/FileConventions/Library.fs"
+#load "../src/FileConventions/Helpers.fs"
+
+let rootDir = Path.Combine(__SOURCE_DIRECTORY__, "..") |> DirectoryInfo
+
+let invalidFiles =
+    Helpers.GetInvalidFiles
+        rootDir
+        "*.fsx"
+        FileConventions.DetectMissingVersionsInNugetPackageReferences
+
+let message =
+    "The following files shouldn't miss the version number in `#r \"nuget: ` refs of F# scripts."
+    + Environment.NewLine
+    + "Please use the exact version of the package instead."
+
+Helpers.AssertNoInvalidFiles invalidFiles message
