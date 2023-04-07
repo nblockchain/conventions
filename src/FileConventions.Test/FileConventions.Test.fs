b607a36a671426a8b9afd31a3ba20880affe0738
realmarv <zahratehraninasab@gmail.com>
2023-04-07 15:40:23 +0330

    FileConventions.Test: add failing test
    Add failing test for WrapText to consider references.
    
    The algorithm shouldn't break the reference lines starting
    with `[number]` pattern.

--- src/FileConventions.Test/FileConventions.Test.fs
+++ src/FileConventions.Test/FileConventions.Test.fs
@@ -287,3 +287,27 @@ let WrapTextTest2() =
     let expectedResult = paragraph
 
     Assert.That(WrapText paragraph characterCount, Is.EqualTo expectedResult)
+
+[<Test>]
+let WrapTextTest3() =
+    let characterCount = 64
+    let tenDigits = "1234567890"
+
+    let seventyChars =
+        tenDigits
+        + tenDigits
+        + tenDigits
+        + tenDigits
+        + tenDigits
+        + tenDigits
+        + tenDigits
+
+    let paragraph =
+        "This is short line referring to [1]."
+        + Environment.NewLine
+        + "[1] someUrl://"
+        + seventyChars
+
+    let expectedResult = paragraph
+
+    Assert.That(WrapText paragraph characterCount, Is.EqualTo expectedResult)
