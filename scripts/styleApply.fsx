6a8965925a0652b798d0c80500349a1ee1627fee
realmarv <zahratehraninasab@gmail.com>
2023-08-02 13:26:37 +0330

    scripts: split styleChecker script
    Split styleChecker into styleCheck and styleApply.

--- scripts/styleApply.fsx
+++ scripts/styleApply.fsx
@@ -1,22 +1,19 @@
 #!/usr/bin/env -S dotnet fsi
 
-open System
-open System.IO
-
 #r "nuget: Mono.Unix, Version=7.1.0-final.1.21458.1"
 #r "nuget: Fsdk, Version=0.6.0--date20230214-0422.git-1ea6f62"
 #load "../src/FileConventions/Config.fs"
 #load "../src/FileConventions/Helpers.fs"
 #load "../src/FileConventions/Library.fs"
 
+open System.IO
+
+open FileConventions
+
 let rootDir = Path.Combine(__SOURCE_DIRECTORY__, "..") |> DirectoryInfo
 
-let invalidFiles =
-    Helpers.GetInvalidFiles
-        rootDir
-        "*.fsx"
-        (fun fileInfo -> not(FileConventions.HasCorrectShebang fileInfo))
-
-Helpers.AssertNoInvalidFiles
-    invalidFiles
-    "The following files should have the correct shebang:"
+StyleFSharpFiles rootDir
+StyleTypeScriptFiles()
+StyleYmlFiles()
+StyleCSharpFiles rootDir
+StyleXamlFiles()
