18b828e9c8f1496f2a520da4ba05831725c832ba
Mehrshad <code.rezaei@gmail.com>
2023-08-24 14:23:45 +0330

    FileConventions: load Helpers.fs first

--- scripts/wrapLatestCommitMsg.fsx
+++ scripts/wrapLatestCommitMsg.fsx
@@ -7,6 +7,7 @@ open System.Text.RegularExpressions
 #r "nuget: Mono.Unix, Version=7.1.0-final.1.21458.1"
 #r "nuget: Fsdk, Version=0.6.0--date20230821-0702.git-5488853"
 
+#load "../src/FileConventions/Helpers.fs"
 #load "../src/FileConventions/Library.fs"
 
 open Fsdk
