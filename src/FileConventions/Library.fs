280d6344e59be7c506c3980c697a8b99976c29ee
Mehrshad <code.rezaei@gmail.com>
2023-09-04 12:30:48 +0330

    FileConventions: improvements to Library.fs
    An assert error message has been added for *.fsx files &
    renaming the func.

--- src/FileConventions/Library.fs
+++ src/FileConventions/Library.fs
@@ -12,6 +12,7 @@ open Fsdk
 let ymlAssertionError = "Bug: file should be a .yml file"
 let projAssertionError = "Bug: file should be a proj file"
 let sourceFileAssertionError = "Bug: file was not a F#/C# source file"
+let fsxAssertionError = "Bug: file was not a F# script source file"
 
 let HasCorrectShebang(fileInfo: FileInfo) =
     let fileText = File.ReadLines fileInfo.FullName
@@ -88,9 +89,7 @@ let DetectAsteriskInPackageReferenceItems(fileInfo: FileInfo) =
     asteriskInPackageReference.IsMatch fileText
 
 let DetectMissingVersionsInNugetPackageReferences(fileInfo: FileInfo) =
-    Misc.BetterAssert
-        (fileInfo.FullName.EndsWith ".fsx")
-        sourceFileAssertionError
+    Misc.BetterAssert (fileInfo.FullName.EndsWith ".fsx") fsxAssertionError
 
     let fileLines = File.ReadLines fileInfo.FullName
 
@@ -320,9 +319,7 @@ let DetectInconsistentVersionsInNugetRefsInFSharpScripts
     =
     fileInfos
     |> Seq.iter(fun fileInfo ->
-        Misc.BetterAssert
-            (fileInfo.FullName.EndsWith ".fsx")
-            sourceFileAssertionError
+        Misc.BetterAssert (fileInfo.FullName.EndsWith ".fsx") fsxAssertionError
     )
 
     DetectInconsistentVersions
@@ -491,7 +488,7 @@ let ContainsConsoleMethods(fileInfo: FileInfo) =
     checkLine fileLines
 
 
-let ReturnAllProjectSourceFile
+let ReturnAllProjectSourceFiles
     (parentDir: DirectoryInfo)
     (patterns: List<string>)
     (shouldFilter: bool)
@@ -521,7 +518,7 @@ let NotFollowingConsoleAppConvention (fileInfo: FileInfo) (shouldFilter: bool) =
 
     if not(fileText.Contains "<OutputType>Exe</OutputType>") then
         let sourceFiles =
-            ReturnAllProjectSourceFile
+            ReturnAllProjectSourceFiles
                 (DirectoryInfo parentDir)
                 [ "*.cs"; "*.fs" ]
                 shouldFilter
