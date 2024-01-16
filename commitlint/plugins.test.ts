bcd7030aaa8be059eddf8599007eb004d6d05a27
Andres G. Aragoneses <knocte@gmail.com>
2024-01-16 11:45:42 +0800

    commitlint: failing test for bug 146

--- commitlint/plugins.test.ts
+++ commitlint/plugins.test.ts
@@ -664,6 +664,23 @@ test("footer-refs-validity7", () => {
     expect(footerRefsValidity7.status).toBe(0);
 });
 
+// This test reflects this issue: https://github.com/nblockchain/conventions/issues/146
+test("footer-refs-validity8", () => {
+    let commitMsgWithCodeBlockAtFooterRef =
+        "foo: blah blah" +
+        "\n\n" +
+        "Blah blah blah[1]." +
+        "\n\n" +
+        "[1]:\n" +
+        "```\n" +
+        "someCodeBlock\n" +
+        "```";
+    let footerRefsValidity8 = runCommitLintOnMsg(
+        commitMsgWithCodeBlockAtFooterRef
+    );
+    expect(footerRefsValidity8.status).toBe(0);
+});
+
 test("prefer-slash-over-backslash1", () => {
     let commitMsgWithBackslash = "foo\\bar: bla bla bla";
     let preferSlashOverBackslash1 = runCommitLintOnMsg(commitMsgWithBackslash);
