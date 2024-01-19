c9dae228d3aa96fd9b80f0c7191272eb99e02222
Andres G. Aragoneses <knocte@gmail.com>
2024-01-19 16:29:17 +0800

    commitlint/plugins: failing test for bullets

--- commitlint/plugins.test.ts
+++ commitlint/plugins.test.ts
@@ -496,6 +496,54 @@ test("body-paragraph-line-min-length8", () => {
     expect(bodyParagraphLineMinLength8.status).toBe(0);
 });
 
+test("body-paragraph-line-min-length9", () => {
+    let commitMsgThatHasAsteriskBullets = `Fixed bug (a title of less than 50 chars)
+
+This is a bullet list of things:
+* Foo.
+* Bar.`;
+
+    let bodyParagraphLineMinLength9 = runCommitLintOnMsg(
+        commitMsgThatHasAsteriskBullets
+    );
+
+    expect(bodyParagraphLineMinLength9.status).toBe(0);
+
+    let commitMsgThatHasDashBullets = `Fixed bug (a title of less than 50 chars)
+
+This is a bullet list of things:
+- Foo.
+- Bar.`;
+
+    let bodyParagraphLineMinLength9Prime = runCommitLintOnMsg(
+        commitMsgThatHasDashBullets
+    );
+
+    expect(bodyParagraphLineMinLength9Prime.status).toBe(0);
+
+    let commitMsgThatOnlyHasAsteriskBullets = `Fixed bug (a title of less than 50 chars)
+
+* Foo.
+* Bar.`;
+
+    let bodyParagraphLineMinLength9DoublePrime = runCommitLintOnMsg(
+        commitMsgThatOnlyHasAsteriskBullets
+    );
+
+    expect(bodyParagraphLineMinLength9DoublePrime.status).toBe(0);
+
+    let commitMsgThatOnlyHasDashBullets = `Fixed bug (a title of less than 50 chars)
+
+- Foo.
+- Bar.`;
+
+    let bodyParagraphLineMinLength9TriplePrime = runCommitLintOnMsg(
+        commitMsgThatOnlyHasDashBullets
+    );
+
+    expect(bodyParagraphLineMinLength9TriplePrime.status).toBe(0);
+});
+
 test("commit-hash-alone1", () => {
     let commitMsgWithCommitUrl = `foo: this is only a title
 
