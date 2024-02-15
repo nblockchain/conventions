748d5772f0a90cfc96a193d184895838e0f9d9df
Andres G. Aragoneses <knocte@gmail.com>
2024-02-15 16:54:27 +0800

    commitlint.sh,CI: install commitlint in 1line

--- commitlint.sh
+++ commitlint.sh
@@ -4,8 +4,7 @@ set -euxo pipefail
 # cd to directory of this script
 cd "$(dirname "$0")"
 npm install
-npm install conventional-changelog-conventionalcommits
-npm install commitlint@latest
+npm install conventional-changelog-conventionalcommits commitlint@latest
 npx commitlint --version
 npx commitlint $@
 cd ..
