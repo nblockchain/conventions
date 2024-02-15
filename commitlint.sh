f1e8c143a45fc9a68dd789a2ef1442d6767be1b4
Andres G. Aragoneses <knocte@gmail.com>
2024-02-15 22:23:53 +0800

    commitlint.sh: simplify
    Rather use more straightforward package names, as advised
    in [1] and [2].
    
    [1] https://github.com/conventional-changelog/commitlint/pull/3892
    [2] https://github.com/Kochava/github-workflows/pull/30

--- commitlint.sh
+++ commitlint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # cd to directory of this script
 cd "$(dirname "$0")"
 npm install
-npm install conventional-changelog-conventionalcommits commitlint@latest
-./node_modules/commitlint/cli.js --version
-./node_modules/commitlint/cli.js $@
+npm install @commitlint/config-conventional @commitlint/cli
+./node_modules/@commitlint/cli/cli.js --version
+./node_modules/@commitlint/cli/cli.js $@
 cd ..
