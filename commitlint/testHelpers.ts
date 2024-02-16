7c18d0aad2d2722138bfcba97cb3aa0b0a4b246c
Andres G. Aragoneses <knocte@gmail.com>
2024-02-16 17:45:46 +0800

    commitlint/testHelpers.ts: FIXME about os.platform

--- commitlint/testHelpers.ts
+++ commitlint/testHelpers.ts
@@ -2,6 +2,9 @@ const { spawnSync } = require("child_process");
 const os = require("os");
 
 export function runCommitLintOnMsg(inputMsg: string) {
+    // FIXME: should we .lowerCase().startsWith("win") in case it starts
+    // returning Win64 in the future? thing is, our CI doesn't like this
+    // change (strangely enough it causes commitlint test suite to fail)
     const command = os.platform() === "win32" ? "npx.cmd" : "npx";
     return spawnSync(command, ["commitlint", "--verbose"], { input: inputMsg });
 }
