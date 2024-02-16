33d908ecd4c05846530be57fea90669d96f2e2e9
Andres G. Aragoneses <knocte@gmail.com>
2024-02-16 18:29:49 +0800

    commitlint.sh: simplify (& sync with tests)
    1. There's actually no need to use npm install to install
    commitlint, just the config-conventional package that our
    configuration depends on. Then commitlint binary can be
    run with npx.
    2. Running it with npx is how integration tests launch it
    so it's better to make this script be in sync with that.

--- commitlint.sh
+++ commitlint.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 # cd to directory of this script
 cd "$(dirname "$0")"
 npm install
-npm install @commitlint/config-conventional @commitlint/cli
-./node_modules/@commitlint/cli/cli.js --version
-./node_modules/@commitlint/cli/cli.js $@
+npm install @commitlint/config-conventional
+npx commitlint --version
+npx commitlint $@
