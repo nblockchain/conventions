871b3170a6a69a68244032a51f133f98e780682f
Andres G. Aragoneses <knocte@gmail.com>
2024-03-18 14:16:27 +0100

    WIP3

--- commitlint.sh
+++ commitlint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # cd to directory of this script
 cd "$(dirname "$0")"
 npm install --verbose
-source ./commitlint/version.cfg
+. ./commitlint/version.cfg
 npm install --verbose @commitlint/config-conventional@$COMMITLINT_VERSION
 npx commitlint --version
 npx commitlint $@
