ec4cb79681ba6dbe6e830dbf99ddb24f24e9b2fb
webwarrior <reg@webwarrior.ws>
2024-11-05 10:24:29 +0100

    scripts: fix unpinnedNugetPackage... script
    Use working dir instead of script's parent directory in
    unpinnedNugetPackageReferenceVersionsInProjects.fsx to
    search for projects as it is intended behavior.

--- scripts/unpinnedNugetPackageReferenceVersionsInProjects.fsx
+++ scripts/unpinnedNugetPackageReferenceVersionsInProjects.fsx
@@ -9,10 +9,13 @@ open System.IO
 #load "../src/FileConventions/Helpers.fs"
 
 let rootDir = Path.Combine(__SOURCE_DIRECTORY__, "..") |> DirectoryInfo
+let currentDir = Directory.GetCurrentDirectory() |> DirectoryInfo
+
+let targetDir = Helpers.PreferLessDeeplyNestedDir currentDir rootDir
 
 let invalidFiles =
     Helpers.GetInvalidFiles
-        rootDir
+        targetDir
         "*.*proj"
         FileConventions.DetectAsteriskInPackageReferenceItems
 
