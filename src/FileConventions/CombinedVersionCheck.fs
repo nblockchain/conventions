81b1fb0d8af101db6e028adfc70039a2a8928755
webwarrior <reg@webwarrior.ws>
2025-04-08 14:53:21 +0200

    Tests: added test for combined version check
    Added test for finding inconsistent package versions across
    both projects and .fsx script files.
    
    Contains failing test.

---
+++ src/FileConventions/CombinedVersionCheck.fs
@@ -0,0 +1,9 @@
+﻿module CombinedVersionCheck
+
+open System.IO
+
+let DetectInconsistentVersionsInNugetRefsInFSharpScripts
+    (_fsxFileInfos: #seq<FileInfo>)
+    (_fsprojFileInfos: #seq<FileInfo>)
+    : bool =
+    raise <| System.NotImplementedException()
