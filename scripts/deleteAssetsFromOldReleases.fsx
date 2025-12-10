1ffc21fac96affb9a2be20c443f61bda40a51ebd
webwarrior-ws <reg@webwarrior.ws>
2025-12-10 11:19:15 +0100

    scripts: add scripts that delete old artifacts
    And releases from Github repository. Scripts copied from [1].
    
    [1] https://github.com/NodeEffectLtd/RunIntoMe/tree/master/scripts

---
+++ scripts/deleteAssetsFromOldReleases.fsx
@@ -0,0 +1,309 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System
+open System.Net
+open System.Net.Http
+open System.Net.Http.Headers
+
+#r "nuget: FSharp.Data, Version=5.0.2"
+open FSharp.Data
+
+#r "nuget: Fsdk, Version=0.6.0--date20230214-0422.git-1ea6f62"
+
+open Fsdk
+open Fsdk.Process
+open Fsdk.FSharpUtil
+
+let githubRepository = Environment.GetEnvironmentVariable "GITHUB_REPOSITORY"
+
+if String.IsNullOrEmpty githubRepository then
+    Console.Error.WriteLine
+        "This script is meant to be used only within a GitHubCI pipeline."
+
+    Environment.Exit 2
+
+let githubToken = Environment.GetEnvironmentVariable "GITHUB_TOKEN"
+
+if String.IsNullOrEmpty githubToken then
+    Console.Error.WriteLine
+        "Please set the GITHUB_TOKEN environment variable in your GitHubCI pipeline:
+    env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    "
+
+    Environment.Exit 2
+
+let numberOfLatestReleasesToKeep = 2
+
+let gitTags =
+    Fsdk
+        .Process
+        .Execute(
+            {
+                Command = "git"
+                Arguments = "tag --sort=creatordate"
+            },
+            Echo.Off
+        )
+        .UnwrapDefault()
+        .Trim()
+        .Split(Environment.NewLine)
+
+let gitTagsStr = String.Join(",", gitTags)
+Console.WriteLine(sprintf "Existing tags in the repository: %s" gitTagsStr)
+
+let gitTagsToRemove =
+    gitTags
+    |> Array.rev
+    |> (fun gitTags -> gitTags.[numberOfLatestReleasesToKeep..])
+
+if Seq.isEmpty gitTagsToRemove then
+    Console.WriteLine "No need to remove any assets from tags."
+else
+    let gitTagsToRemoveStr = String.Join(",", gitTagsToRemove)
+
+    Console.WriteLine(
+        sprintf "Tags to be purged of assets: %s" gitTagsToRemoveStr
+    )
+
+type GitHubAssetType =
+    JsonProvider<"""{
+  "url": "https://api.github.com/repos/aaarani/RunIntoMe/releases/100938445",
+  "assets_url": "https://api.github.com/repos/aaarani/RunIntoMe/releases/100938445/assets",
+  "upload_url": "https://uploads.github.com/repos/aaarani/RunIntoMe/releases/100938445/assets{?name,label}",
+  "html_url": "https://github.com/aaarani/RunIntoMe/releases/tag/0.19",
+  "id": 100938445,
+  "author": {
+    "login": "github-actions[bot]",
+    "id": 41898282,
+    "node_id": "MDM6Qm90NDE4OTgyODI=",
+    "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+    "html_url": "https://github.com/apps/github-actions",
+    "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+    "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+    "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+    "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+    "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+    "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+    "type": "Bot",
+    "site_admin": false
+  },
+  "node_id": "RE_kwDOJbCwLc4GBDLN",
+  "tag_name": "0.19",
+  "target_commitish": "18f22bbfc8e7d9987518b41c6c19f93493e1967a",
+  "name": "Release 0.19",
+  "draft": false,
+  "prerelease": false,
+  "created_at": "2023-04-26T10:10:01Z",
+  "published_at": "2023-04-26T11:22:47Z",
+  "assets": [
+    {
+      "url": "https://api.github.com/repos/aaarani/RunIntoMe/releases/assets/105368692",
+      "id": 105368692,
+      "node_id": "RA_kwDOJbCwLc4GR8x0",
+      "name": "com.companyname.runintome-Signed.apk",
+      "label": "",
+      "uploader": {
+        "login": "github-actions[bot]",
+        "id": 41898282,
+        "node_id": "MDM6Qm90NDE4OTgyODI=",
+        "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+        "html_url": "https://github.com/apps/github-actions",
+        "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+        "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+        "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+        "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+        "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+        "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+        "type": "Bot",
+        "site_admin": false
+      },
+      "content_type": "application/vnd.android.package-archive",
+      "state": "uploaded",
+      "size": 48707293,
+      "download_count": 0,
+      "created_at": "2023-04-26T11:34:22Z",
+      "updated_at": "2023-04-26T11:34:23Z",
+      "browser_download_url": "https://github.com/aaarani/RunIntoMe/releases/download/0.19/com.companyname.runintome-Signed.apk"
+    },
+    {
+      "url": "https://api.github.com/repos/aaarani/RunIntoMe/releases/assets/105366781",
+      "id": 105366781,
+      "node_id": "RA_kwDOJbCwLc4GR8T9",
+      "name": "runIntoMe-db.zip",
+      "label": "",
+      "uploader": {
+        "login": "github-actions[bot]",
+        "id": 41898282,
+        "node_id": "MDM6Qm90NDE4OTgyODI=",
+        "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+        "html_url": "https://github.com/apps/github-actions",
+        "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+        "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+        "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+        "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+        "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+        "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+        "type": "Bot",
+        "site_admin": false
+      },
+      "content_type": "application/zip",
+      "state": "uploaded",
+      "size": 369,
+      "download_count": 0,
+      "created_at": "2023-04-26T11:22:50Z",
+      "updated_at": "2023-04-26T11:22:50Z",
+      "browser_download_url": "https://github.com/aaarani/RunIntoMe/releases/download/0.19/runIntoMe-db.zip"
+    },
+    {
+      "url": "https://api.github.com/repos/aaarani/RunIntoMe/releases/assets/105366778",
+      "id": 105366778,
+      "node_id": "RA_kwDOJbCwLc4GR8T6",
+      "name": "runIntoMe-server-linux-amd64.zip",
+      "label": "",
+      "uploader": {
+        "login": "github-actions[bot]",
+        "id": 41898282,
+        "node_id": "MDM6Qm90NDE4OTgyODI=",
+        "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+        "html_url": "https://github.com/apps/github-actions",
+        "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+        "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+        "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+        "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+        "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+        "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+        "type": "Bot",
+        "site_admin": false
+      },
+      "content_type": "application/zip",
+      "state": "uploaded",
+      "size": 42104852,
+      "download_count": 0,
+      "created_at": "2023-04-26T11:22:48Z",
+      "updated_at": "2023-04-26T11:22:49Z",
+      "browser_download_url": "https://github.com/aaarani/RunIntoMe/releases/download/0.19/runIntoMe-server-linux-amd64.zip"
+    }
+  ],
+  "tarball_url": "https://api.github.com/repos/aaarani/RunIntoMe/tarball/0.19",
+  "zipball_url": "https://api.github.com/repos/aaarani/RunIntoMe/zipball/0.19",
+  "body": "Backend"
+}""">
+
+let userAgent = ".NET App"
+let xGitHubApiVersion = "2022-11-28"
+
+let githubApiCallForbiddenErrorMsg =
+    """GITHUB_TOKEN passed doesn't seem to have enough permissions.
+To modify the permissions of your token, navigate to the Settings section of your 
+repository or organization and click on Actions button, then select General. From 
+'Workflow permissions' section on that page, choose 'Read and write permissions' 
+(which grants access to content and the ability to make changes).
+"""
+
+gitTagsToRemove
+|> Seq.iter(fun gitTagToRemove ->
+    let maybeReleaseJsonString: Option<string> =
+        let mediaTypeWithQuality =
+            MediaTypeWithQualityHeaderValue "application/vnd.github+json"
+
+        use client = new HttpClient()
+        client.DefaultRequestHeaders.Accept.Clear()
+        client.DefaultRequestHeaders.Accept.Add mediaTypeWithQuality
+        client.DefaultRequestHeaders.Add("User-Agent", userAgent)
+
+        client.DefaultRequestHeaders.Add(
+            "X-GitHub-Api-Version",
+            xGitHubApiVersion
+        )
+
+        if not(String.IsNullOrEmpty githubToken) then
+            client.DefaultRequestHeaders.Add(
+                "Authorization",
+                $"Bearer {githubToken}"
+            )
+
+        let url =
+            $"https://api.github.com/repos/{githubRepository}/releases/tags/{gitTagToRemove}"
+
+        try
+            let task = client.GetStringAsync url
+            let result = Async.AwaitTask task |> Async.RunSynchronously
+            Some result
+        with
+        | ex ->
+            match FindException<HttpRequestException> ex with
+            | Some httpRequestException ->
+                match httpRequestException.StatusCode |> Option.ofNullable with
+                | Some statusCode when statusCode = HttpStatusCode.Forbidden ->
+
+                    failwith githubApiCallForbiddenErrorMsg
+
+                | Some statusCode when statusCode = HttpStatusCode.NotFound ->
+                    None
+
+                | _ -> reraise()
+
+            | _ -> reraise()
+
+    let DeleteAsset (url: string) (accessToken: string) =
+        use client = new HttpClient()
+        client.DefaultRequestHeaders.Accept.Clear()
+        client.DefaultRequestHeaders.Add("User-Agent", userAgent)
+
+        client.DefaultRequestHeaders.Add(
+            "X-GitHub-Api-Version",
+            xGitHubApiVersion
+        )
+
+        if not(String.IsNullOrEmpty accessToken) then
+            client.DefaultRequestHeaders.Add(
+                "Authorization",
+                $"Bearer {accessToken}"
+            )
+
+        try
+            let task = client.DeleteAsync url
+
+            Async.AwaitTask task
+            |> Async.RunSynchronously
+            |> ignore<HttpResponseMessage>
+
+        with
+        | ex ->
+            match FindException<HttpRequestException> ex with
+            | Some httpRequestException ->
+                match httpRequestException.StatusCode |> Option.ofNullable with
+                | Some statusCode when statusCode = HttpStatusCode.Forbidden ->
+                    failwith githubApiCallForbiddenErrorMsg
+                | _ -> reraise()
+            | _ -> reraise()
+
+    match maybeReleaseJsonString with
+    | Some releaseJsonString ->
+        let parsedJsonObj = GitHubAssetType.Parse releaseJsonString
+
+        parsedJsonObj.Assets
+        |> Seq.iter(fun asset -> DeleteAsset asset.Url githubToken)
+    | _ -> ()
+)
---
+++ scripts/deleteOldArtifacts.fsx
@@ -0,0 +1,263 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System
+open System.Net
+open System.Net.Http
+open System.Net.Http.Headers
+
+#r "nuget: FSharp.Data, Version=5.0.2"
+open FSharp.Data
+
+#r "nuget: Fsdk, Version=0.6.0--date20230214-0422.git-1ea6f62"
+
+open Fsdk
+open Fsdk.Process
+open Fsdk.FSharpUtil
+
+let githubRepository = Environment.GetEnvironmentVariable "GITHUB_REPOSITORY"
+
+if String.IsNullOrEmpty githubRepository then
+    Console.Error.WriteLine
+        "This script is meant to be used only within a GitHubCI pipeline."
+
+    Environment.Exit 1
+
+let githubToken = Environment.GetEnvironmentVariable "GITHUB_TOKEN"
+
+if String.IsNullOrEmpty githubToken then
+    Console.Error.WriteLine
+        "Please set the GITHUB_TOKEN environment variable in your GitHubCI pipeline:
+
+    env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    "
+
+    Environment.Exit 2
+
+let GetPreviousCommitsHashes() =
+    Fsdk
+        .Process
+        .Execute(
+            {
+                Command = "git"
+                Arguments = "log --pretty=format:%H"
+            },
+            Echo.Off
+        )
+        .UnwrapDefault()
+        .Trim()
+        .Split(
+        Environment.NewLine
+    ).[1..]
+
+type GithubArtifactsType =
+    JsonProvider<"""
+{
+  "total_count": 1,
+  "artifacts": [
+    {
+      "id": 667505922,
+      "node_id": "MDg6QXJ0aWZhY3Q2Njc1MDU5MjI=",
+      "name": "publishedPackages",
+      "size_in_bytes": 151303891,
+      "url": "https://api.github.com/repos/aaarani/RunIntoMe/actions/artifacts/667505922",
+      "archive_download_url": "https://api.github.com/repos/aaarani/RunIntoMe/actions/artifacts/667505922/zip",
+      "expired": false,
+      "created_at": "2023-04-26T22:57:37Z",
+      "updated_at": "2023-04-26T22:57:38Z",
+      "expires_at": "2023-07-25T22:47:52Z",
+      "workflow_run": {
+        "id": 4813959132,
+        "repository_id": 632336429,
+        "head_repository_id": 632336429,
+        "head_branch": "wip/WelcomePage",
+        "head_sha": "1357c79f43c12189c7aa4c46b5661400b704c211"
+      }
+    }
+  ]
+}
+""">
+
+let githubApiCallForbiddenErrorMsg =
+    """GITHUB_TOKEN passed doesn't seem to have enough permissions.
+
+To modify the permissions of your token, navigate to the Settings section of your 
+repository or organization and click on Actions button, then select General. From 
+'Workflow permissions' section on that page, choose 'Read and write permissions' 
+(which grants access to content and the ability to make changes).
+"""
+
+type ApiAction =
+    | Get
+    | Delete
+
+let GitHubApiQuery
+    (url: string)
+    (action: ApiAction)
+    (acceptMediaTypeOpt: Option<string>)
+    =
+    let userAgent = ".NET App"
+    let xGitHubApiVersion = "2022-11-28"
+
+    use client = new HttpClient()
+    client.DefaultRequestHeaders.Accept.Clear()
+
+    match acceptMediaTypeOpt with
+    | Some acceptMediaType ->
+        client.DefaultRequestHeaders.Accept.Add(
+            MediaTypeWithQualityHeaderValue acceptMediaType
+        )
+    | None -> ()
+
+    client.DefaultRequestHeaders.Add("User-Agent", userAgent)
+    client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", xGitHubApiVersion)
+
+    if not(String.IsNullOrEmpty githubToken) then
+        client.DefaultRequestHeaders.Add(
+            "Authorization",
+            $"Bearer {githubToken}"
+        )
+
+    Console.WriteLine(sprintf "Calling %s ..." url)
+
+    try
+        match action with
+        | Get ->
+            client.GetStringAsync url
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        | Delete ->
+            (client.DeleteAsync url |> Async.AwaitTask |> Async.RunSynchronously)
+                .ToString()
+    with
+    | ex ->
+        match FindException<HttpRequestException> ex with
+        | Some httpRequestException ->
+            match httpRequestException.StatusCode |> Option.ofNullable with
+            | Some statusCode when statusCode = HttpStatusCode.Forbidden ->
+
+                failwith githubApiCallForbiddenErrorMsg
+
+            | _ -> reraise()
+
+        | _ -> reraise()
+
+let hasCIStatus(commit: string) : bool =
+    let url =
+        sprintf
+            "https://api.github.com/repos/%s/commits/%s/check-suites"
+            githubRepository
+            commit
+
+    let mediaTypeWithQuality = "application/vnd.github+json" |> Some
+
+    let json = GitHubApiQuery url Get mediaTypeWithQuality
+
+    not(json.Contains "\"check_suites\":[]")
+
+let userNameOrOrgName = githubRepository.Split("/").[0]
+
+let artifactsApiQueryResult =
+    let mediaTypeWithQuality = "application/vnd.github+json" |> Some
+
+    let url =
+        $"https://api.github.com/repos/{githubRepository}/actions/artifacts"
+
+    GitHubApiQuery url Get mediaTypeWithQuality
+
+let parsedJsonObj = GithubArtifactsType.Parse artifactsApiQueryResult
+
+let artifactIds = parsedJsonObj.Artifacts
+
+type WhatArtifactsToDelete =
+    | AllFromBranchlessCommitsAndAllFromPreviousOnesInThisBranch
+
+    // FIXME: there is an edge case, e.g. if previous CI commit is red
+    // (such as "Empty commit to test CI") then the next commit that fixes
+    // CI will not remove the artifacts from the previous-previous commit
+    | OnlyOneFromPreviousCiStatus
+
+let whatArtifactsToDelete =
+    AllFromBranchlessCommitsAndAllFromPreviousOnesInThisBranch
+
+let DoesCommitExistInTheRepoAndBranch(commit: string) =
+    let branchesTheCommitBelongsTo =
+        try
+            Fsdk
+                .Process
+                .Execute(
+                    {
+                        Command = "git"
+                        Arguments = $"branch --contains {commit}"
+                    },
+                    Echo.Off
+                )
+                .UnwrapDefault()
+                .Trim()
+        with
+        | :? ProcessFailed -> String.Empty
+        | _ -> reraise()
+
+    not(String.IsNullOrEmpty branchesTheCommitBelongsTo)
+
+let DeleteArtifact (githubRepository: string) (artifactId: string) =
+    let url =
+        $"https://api.github.com/repos/{githubRepository}/actions/artifacts/{artifactId}"
+
+    GitHubApiQuery url Delete None |> ignore
+
+let previousCommitsHashes = GetPreviousCommitsHashes()
+
+match whatArtifactsToDelete with
+| OnlyOneFromPreviousCiStatus ->
+
+    Console.WriteLine "About to delete only the artifacts of previous commit..."
+
+    let previousCommitWithCIStatusOpt =
+        Seq.tryFind hasCIStatus previousCommitsHashes
+
+    match previousCommitWithCIStatusOpt with
+    | None ->
+        Console.WriteLine "No commits found with CI status"
+        exit 0
+    | _ -> ()
+
+    let previousCommitWithCIStatus = previousCommitWithCIStatusOpt.Value
+
+    let artifactIdsToDelete =
+        artifactIds
+        |> Seq.filter(fun item ->
+            item.WorkflowRun.HeadSha = previousCommitWithCIStatus
+        )
+        |> Seq.map(fun artifact -> artifact.Id.ToString())
+
+    artifactIdsToDelete
+    |> Seq.iter(fun artifactId -> DeleteArtifact githubRepository artifactId)
+
+| AllFromBranchlessCommitsAndAllFromPreviousOnesInThisBranch ->
+    Console.WriteLine "About to delete all previous artifacts of this branch..."
+
+    let artifactIdsToDelete =
+        artifactIds
+        |> Seq.filter(fun item ->
+            let commit = item.WorkflowRun.HeadSha
+            Seq.contains commit previousCommitsHashes
+        )
+        |> Seq.map(fun artifact -> artifact.Id.ToString())
+
+    artifactIdsToDelete
+    |> Seq.iter(fun artifactId -> DeleteArtifact githubRepository artifactId)
+
+    Console.WriteLine
+        "About to delete all the artifacts of branchless commits..."
+
+    let orphanArtifactIdsToDelete =
+        artifactIds
+        |> Seq.filter(fun item ->
+            let commit = item.WorkflowRun.HeadSha
+            not(DoesCommitExistInTheRepoAndBranch commit)
+        )
+        |> Seq.map(fun artifact -> artifact.Id.ToString())
+
+    orphanArtifactIdsToDelete
+    |> Seq.iter(fun artifactId -> DeleteArtifact githubRepository artifactId)
