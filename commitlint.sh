c25726ac4ffe6ceaab66323f51dc7e362c129555
Andres G. Aragoneses <knocte@gmail.com>
2026-01-31 18:34:10 +0800

    commitlint.sh: use `source` instead of `.`
    The latter is less readable.

--- commitlint.sh
+++ commitlint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # cd to directory of this script
 cd "$(dirname "$0")"
 npm install --verbose
-. ./commitlint/version.config
+source ./commitlint/version.config
 npm install --verbose @commitlint/config-conventional@$COMMITLINT_VERSION
 npx commitlint --version
 npx commitlint $@
