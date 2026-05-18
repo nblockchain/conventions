731908b94eb1443f4f65b0f519eb620cdc7ecb4f
Andrés G. Aragoneses <knocte@gmail.com>
2026-05-18 18:23:52 +0800

    scripts(format.sh),package.json: --check support
    Same motivation as previous commit.

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,5 +5,5 @@ which npx || (echo '\nPlease install `npx`, maybe installing NPM?' >&2 && exit 1
 
 npx --no-install prettier --version || (echo '\nPlease install `prettier` via `npm install` first; if this problem persists, try `npm rebuild` or `git clean -fdx` before `npm install`' >&2 && exit 1)
 
-npx --no-install prettier --quote-props=consistent --write './**/*.{yml,ts}'
+npx --no-install prettier $@ --quote-props=consistent './**/*.{yml,ts}'
 
