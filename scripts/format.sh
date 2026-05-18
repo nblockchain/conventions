812edc6828695df9ffb96e6df8cc913862ef0fea
Andrés G. Aragoneses <knocte@gmail.com>
2026-05-18 16:59:54 +0800

    scripts,package.json: mv format logic to .sh file
    This way:
    * The logic is more readable.
    * It fixes execution on MINGW64(Windows) terminal.

---
+++ scripts/format.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+which npx || (echo '\nPlease install `npx`, maybe installing NPM?' >&2 && exit 1)
+
+npx --no-install prettier --version || (echo '\nPlease install `prettier` via `npm install` first; if this problem persists, try `npm rebuild` or `git clean -fdx` before `npm install`' >&2 && exit 1)
+
+npx --no-install prettier --quote-props=consistent --write './**/*.{yml,ts}'
+
